40a709e53ead10c733faecdbc8957433268a6c27
David Taylor <>
2024-07-03 12:43:54 +1000

    Initial commit of working WarGames screen simulator

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # trsmutableiii
-Python scripts for the Transmutable tiny TRS-80 Model III
+
+Python scripts for [Trevor Flowers' Electronic Tiny TRS-80 Model III](https://store.transmutable.com/l/e-trs-80-model-iii).
+
+## Overview
+
+These Python scripts are intended for [Trevor Flowers' Electronic Tiny TRS-80 Model III](https://store.transmutable.com/l/e-trs-80-model-iii).
+
+It just so happens that they're specifically designed to run within that 1:6 scale model; screen orientation matters and the model covers some of the screen! However, even if you do not have that particular model, you might find these scripts useful for your own projects on that same hardware.
+
+These scripts depend on the Adafruit QT Py microcontroller and the Adafruit Monochrome 1.3" 128x64 OLED graphic display. 
+
+## Prerequisites
+
+To run the scripts in this repository, you will need the following hardware:
+- [Adafruit QT Py ESP32-S3 WiFi Dev Board with STEMMA QT - 8 MB Flash / No PSRAM](https://learn.adafruit.com/adafruit-qt-py-esp32-s3)
+- [Adafruit 240x240 Wide Angle TFT LCD display with MicroSD](https://learn.adafruit.com/adafruit-1-3-and-1-54-240-x-240-wide-angle-tft-lcd-displays)
+
+Or, simply purchase a complete [1:6 scale model of a TRS-80 Model III](https://store.transmutable.com/l/e-trs-80-model-iii), which includes the microcontroller and display:
+
+![alt text](e-trs-80-model-iii.png)
+
+## Getting Started
+
+To get started, clone this repository to your local machine:
+
+```bash
+git clone git@github.com:cloudartisan/trsmutableiii.git
+cd trsmutableiii
+```
+## Scripts
+
+### wargames
+
+This script is a simple implementation of the computer screens seen in the movie WarGames, all from David Lightman's point of view. As much as possible, I've tried to mimic the screens from the movie. However, the display limitations sometimes made it necessary to condense/abbreviate parts of the screens.
+
+The screens are displayed in blue to match the movie (notice that most of the computer text in the movie is blue, not monochrome green, white, or amber).
+
+The screens simulate a typing / baud rate rather than immediately displaying the text.
+
+#### Install
+
+To install the script, copy the _content_ of the `wargames` directory to the `CIRCUITPY` drive on your device.
+
+For example, on a Mac:
+
+```bash
+cp -r wargames/* /Volumes/CIRCUITPY/
+```
+
+The auto-reload should kick in and the program should start running on the device.
+
+
+## Debugging via Serial Console
+To debug the device via the serial console, follow the instructions below.
+
+### Mac or Linux
+Follow the instructions [here](https://learn.adafruit.com/welcome-to-circuitpython/advanced-serial-console-on-mac-and-linux) to set up the serial console on Mac or Linux.
+
+### Windows
+Follow the instructions [here](https://learn.adafruit.com/welcome-to-circuitpython/advanced-serial-console-on-windows) to set up the serial console on Windows.
+
+### VSCode/Mu/Arduino IDE
+
+If you use VSCode, you can install the CircuitPython extension and use its built-in serial console. After installing the extension, at the bottom of your VSCode window you will need to:
+- Choose Circuit Python Board (in my case, `Adafruit:QT Py ESP32S3 no psram`)
+- Select Serial Port (in my case, it looked like `/dev/tty.usbmodemXXXXXXX`)
+
+If you use the [Mu editor](https://codewith.mu/), you can [use the serial console built into the editor](https://codewith.mu/en/tutorials/1.2/circuitpython).
+
+If you use the [Arduino IDE](https://www.arduino.cc/en/software), you can use the serial monitor built into the IDE. You will first need to install the appropriate CircuitPython board and libraries in the Arduino IDE. This _should_ happen automatically if your device is connected to the computer and the Arduino IDE is open.
+
+### REPL
+
+Depending on how you've accessed the serial console, you might be able to access the REPL. Using the REPL you can run Python commands directly on the device for further debugging.
+
+In my case, I connected via the serial console using `screen` then used `Ctrl-C` to interrupt the running program and access the REPL:
+
+```
+KeyboardInterrupt:
+
+Code done running.
+
+Press any key to enter the REPL. Use CTRL-D to reload.
+
+Adafruit CircuitPython 8.2.6 on 2023-09-12; Adafruit QT Py ESP32-S3 no psram with ESP32S3
+>>>
+```
+
+## Upgrading CircuitPython
+
+At the time of writing, the latest stable version of CircuitPython is 9.0.5. The version that was already installed on my device was 8.2.6.
+
+You can check the latest releases [here](https://github.com/adafruit/circuitpython/releases).
+
+If you decide to upgrade CircuitPython on your device, follow the instructions [here](https://circuitpython.org/board/adafruit_qtpy_esp32s3_nopsram/).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ wargames/code.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+
+import board
+import displayio
+import gc
+import terminalio
+from adafruit_display_text import label
+from adafruit_st7789 import ST7789
+import time
+import json
+
+# Constants for display configuration
+LEFT_MARGIN = 10
+RIGHT_MARGIN = 10
+TOP_MARGIN = 20
+FONT_SCALE = 1
+CHAR_WIDTH = 6  # Adjust for your font size
+LINE_HEIGHT = 15  # Adjust for your font size
+DEFAULT_TEXT_COLOUR = (0, 255, 255)  # Default colour (RGB format)
+
+# Physical display configuration to be initialised
+SPI = board.SPI()
+TFT_CS = board.D5  # Chip select pin
+TFT_DC = board.D16  # Data/command pin
+TFT_RESET = board.D9  # Reset pin
+
+DEFAULT_DEBUG_DELAY=1
+DEFAULT_SCREEN_DELAY=4
+DEFAULT_CHAR_DELAY=0.025
+DEFAULT_LOWER_DELAY=0.25
+DEFAULT_UPPER_DELAY=0.005
+
+
+def debug_print(*lines, delay=DEFAULT_DEBUG_DELAY):
+    """Print debug messages with an optional delay"""
+    for line in lines:
+        print(line)
+        time.sleep(delay)
+
+
+def debug_board():
+    """Print debug information about the board"""
+    debug_print(f"Board name: {board.board_id}", delay=10)
+
+
+def clear_display(display):
+    debug_print("Clearing display...")
+    splash = displayio.Group()
+    display.show(splash)
+    gc.collect()  # Explicitly collect garbage after clearing display
+    return splash
+
+
+def load_screens(file_path):
+    """Load screens from a JSON file"""
+    with open(file_path, 'r') as file:
+        screens = json.load(file)
+    return screens
+
+
+def wrap_text(text, max_chars):
+    """Wrap text to fit within max_chars per line"""
+    # If the text is already short enough, return it unmodified as a single line
+    if len(text) <= max_chars:
+        return [text]
+    # If the text is too long, wrap it to fit within the specified width, while
+    # stripping unnecessary whitespace
+    words = text.split(' ')
+    wrapped_lines = []
+    line = ''
+    for word in words:
+        if len(line) + len(word) + 1 <= max_chars:
+            line += (word + ' ')
+        else:
+            wrapped_lines.append(line.strip())
+            line = word + ' '
+    if line:
+        wrapped_lines.append(line.strip())
+    return wrapped_lines
+
+
+def display_line_with_typing(splash, text, x, y, scale, colour, upper_delay, lower_delay):
+    """Display a single line with a typing effect"""
+    text_area = label.Label(terminalio.FONT, text="", x=x, y=y, scale=scale, color=colour)
+    splash.append(text_area)
+
+    for char in text:
+        char_delay = DEFAULT_CHAR_DELAY
+        if char.islower():
+            char_delay = lower_delay
+        elif char.isupper():
+            char_delay = upper_delay
+
+        text_area.text += char
+        time.sleep(char_delay)
+
+    return y + LINE_HEIGHT  # Advance to the next line position
+
+
+def format_and_display_line(display, splash, left_text, centre_text, right_text, y, scale, colour, upper_delay, lower_delay):
+    """Format and display a line with left, centre, and right alignment"""
+    total_chars = (display.width - LEFT_MARGIN - RIGHT_MARGIN) // CHAR_WIDTH
+    left_lines = wrap_text(left_text, total_chars)
+    centre_lines = wrap_text(centre_text, total_chars)
+    right_lines = wrap_text(right_text, total_chars)
+
+    max_lines = max(len(left_lines), len(centre_lines), len(right_lines))
+
+    for i in range(max_lines):
+        left_part = left_lines[i] if i < len(left_lines) else ""
+        centre_part = centre_lines[i] if i < len(centre_lines) else ""
+        right_part = right_lines[i] if i < len(right_lines) else ""
+
+        if centre_part:
+            centre_start = (total_chars - len(centre_part)) // 2
+            left_pad = centre_start - len(left_part)
+            right_pad = total_chars - (centre_start + len(centre_part) + len(right_part))
+        else:
+            left_pad = 0
+            right_pad = total_chars - len(left_part) - len(right_part)
+
+        padded_left_text = left_part + ' ' * left_pad
+        padded_right_text = ' ' * right_pad + right_part
+        full_text = padded_left_text + centre_part + padded_right_text
+
+        y = display_line_with_typing(splash, full_text, LEFT_MARGIN, y, scale, colour, upper_delay, lower_delay)
+
+    return y
+
+
+def display_screen_with_typing(display, screen):
+    """Display all lines of a screen with a typing effect"""
+    debug_print("Displaying screen with typing effect")
+    splash = clear_display(display)
+
+    lines = screen["lines"]
+    scale = screen.get("scale", FONT_SCALE)
+    colour = tuple(screen.get("colour", DEFAULT_TEXT_COLOUR))
+    upper_delay = screen.get("upper_delay", DEFAULT_UPPER_DELAY)
+    lower_delay = screen.get("lower_delay", DEFAULT_LOWER_DELAY)
+    line_delays = screen.get("line_delays", {})
+    y = TOP_MARGIN
+
+    for idx, (left_text, centre_text, right_text, *delay) in enumerate(lines):
+        debug_print(f"Displaying line {idx}: {left_text}, {centre_text}, {right_text}")
+        y = format_and_display_line(display, splash, left_text, centre_text, right_text, y, scale, colour, upper_delay, lower_delay)
+        if delay:
+            time.sleep(delay[0])
+        elif idx in line_delays:
+            time.sleep(line_delays[idx])
+
+    debug_print("Displayed all lines of the screen with typing effect")
+
+
+def display_screens(display):
+    """
+    Loop through every screen, displaying them with typing effect, delay
+    after each screen, then clear the display, repeat ad infinitum.
+    """
+    screens = load_screens('./screens.json')
+    while True:
+        for screen in screens:
+            try:
+                display_screen_with_typing(display, screen)
+            except Exception as e:
+                debug_print(f"Error displaying screen: {e}", delay=5)
+            time.sleep(screen.get("delay", DEFAULT_SCREEN_DELAY))
+
+
+def initialise_display():
+    try:
+        displayio.release_displays()
+        display_bus = displayio.FourWire(
+            SPI,
+            command=TFT_DC,
+            chip_select=TFT_CS,
+            reset=TFT_RESET
+        )
+        display = ST7789(
+            display_bus,
+            width=280,
+            height=240,
+            rowstart=20,
+            rotation=270
+        )
+        debug_print("Display initialised successfully")
+    except Exception as e:
+        debug_print(f"Error initialising display: {e}", delay=5)
+        raise e
+
+    # Display properties
+    debug_print(
+        f"Display width: {display.width}",
+        f"Display height: {display.height}",
+        f"Display rotation: {display.rotation}"
+    )
+    return display
+
+
+def main():
+    debug_board()
+    display = initialise_display()
+    display_screens(display)
+
+
+if __name__ == "__main__":
+    main()
